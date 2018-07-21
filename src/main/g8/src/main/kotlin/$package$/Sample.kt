fdb85d60a897ac7c33d14c3196a6ab8a10f42b1a
Hervé Darritchon <herve.darritchon@gmail.com>
2018-07-21 16:21:21 +0200

    Initial commit

---
+++ src/main/g8/src/main/kotlin/$package$/Sample.kt
@@ -0,0 +1,18 @@
+package $package$
+
+/**
+ * Created by Hervé Darritchon on 18/06/2018.
+ *
+ */
+
+class Sample {
+
+    fun aMethod (param : String) : String {
+        return param
+    }
+
+    fun main(args: Array<String>) {
+        println("This is a sample !!")
+    }
+
+}
